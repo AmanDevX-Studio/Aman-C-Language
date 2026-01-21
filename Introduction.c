3e0768210ffb388e43fba40f8a09733f98f39a36
AmanDevX-Studio <akhilkumar72147@gmail.com>
2026-01-22 00:45:37 +0530

    commited

---
+++ Introduction.c
@@ -0,0 +1,10 @@
+// Introduction Of c Language
+#include <stdio.h> // header file
+
+// basic sysntax of c langauage to use Every time
+int main()
+{
+  // this is a ouput fuction
+  printf("Welcome Aman Into C Programing");
+  return 0;
+}
