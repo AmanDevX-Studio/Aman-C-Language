c1c3475eabd24f945b74857f7525cb182ab29e26
AmanDevX-Studio <akhilkumar72147@gmail.com>
2026-01-22 02:27:03 +0530

    commited

---
+++ 02_Addition_Numbers.c
@@ -0,0 +1,17 @@
+// Introduction Of c Language
+
+#include <stdio.h>
+int main()
+{
+
+  int a, b, sum;
+  system("cls"); // Program start and screen clear
+
+  printf("Enter two numbers : ");
+  scanf("%d %d", &a, &b);
+
+  sum = a + b;
+  printf("Addtion of two number is %d : \n", sum);
+
+  return 0;
+}
---
+++ 03_multiplication_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, mullti;
+    system("cls");
+        printf("Enter Two Number : ");
+    scanf("%d %d", &a, &b);
+
+    mullti = a * b;
+    printf("Multiplication of two number : %d\n", mullti);
+    return 0;
+}
